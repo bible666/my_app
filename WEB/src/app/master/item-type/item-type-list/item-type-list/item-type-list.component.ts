993cbb482e8b806074ba987c1e1e8b9d5089f595
rattapol <bible6666@hotmail.com>
2019-04-19 19:39:04 +0700

    add copy code from unit list to item type list

--- WEB/src/app/master/item-type/item-type-list/item-type-list/item-type-list.component.ts
+++ WEB/src/app/master/item-type/item-type-list/item-type-list/item-type-list.component.ts
@@ -1,15 +1,216 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { MatTableDataSource } from '@angular/material';
+import { ItemTypeService, cSearch } from '../../item-type.service';
+import {merge, Observable, of as observableOf} from 'rxjs';
+import {catchError, map, startWith, switchMap} from 'rxjs/operators';
+import {FormControl, FormGroup} from '@angular/forms';
+import { OriginalListComponent } from 'src/app/original/original-list/original-list.component';
+import { Router } from '@angular/router';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
+import { ShowDialogComponent } from '../../../../common/show-dialog/show-dialog.component'
+import { MatSnackBar, MatSnackBarConfig } from '@angular/material';
+import { MyMessageComponent } from '../../../../common/my-message/my-message.component'
 
 @Component({
   selector: 'app-item-type-list',
   templateUrl: './item-type-list.component.html',
   styleUrls: ['./item-type-list.component.css']
 })
-export class ItemTypeListComponent implements OnInit {
+export class ItemTypeListComponent extends OriginalListComponent {
 
-  constructor() { }
+	//----------------------------------------------------------------
+	// set for grid
+	//----------------------------------------------------------------
+	displayedColumns: string[]		= ['unit_code','unit_name','update','delete'];
+	
+	//----------------------------------------------------------------
+	// set datasort for show in list
+	//----------------------------------------------------------------
+	dataSource:MatTableDataSource<ListElement>;
 
-  ngOnInit() {
-  }
+	//----------------------------------------------------------------
+	// Set Valiable for use in form
+	//----------------------------------------------------------------
+	searchForm:FormGroup;
+	unitCode:string;
+	unitName:string;
 
+	private configError: MatSnackBarConfig = {
+		panelClass: ['style-error'],
+		duration: 2000,
+	};
+
+
+	//-----------------------------------------------------------------
+	// constructor class
+	//-----------------------------------------------------------------
+	constructor(private unitS: ItemTypeService, private router: Router, 
+		public dialog: MatDialog,private snackBar: MatSnackBar) {
+		super();
+	 }
+
+	//----------------------------------------------------------------
+	// Clear Local Store Data
+	//----------------------------------------------------------------
+	private _clear_data(){
+		localStorage.setItem('unit_search.unit_code','');
+		localStorage.setItem('unit_search.unit_name','');
+	}
+
+	//----------------------------------------------------------------
+	// set search data from local store data
+	//----------------------------------------------------------------
+	protected _set_search_data(){
+		let search_data: cSearch = new cSearch();
+
+		search_data.sort_column		= this.sort.active;
+		search_data.sort_direction	= this.sort.direction;
+		search_data.page_index		= this.paginator.pageIndex;
+		if (this.paginator.pageSize == null){
+			search_data.page_size		= this.AR_pages[0];
+		}else {
+			search_data.page_size		= this.paginator.pageSize;
+		}
+		search_data.unit_code		= this.unitCode;
+		search_data.unit_name		= this.unitName;
+
+		return this.unitS.getListData(search_data);
+	}
+
+	//----------------------------------------------------------------
+	// set init data from local store
+	//----------------------------------------------------------------
+	protected _set_init(){
+		this.unitCode =  localStorage.getItem('unit_search.unit_code');
+		this.unitName =  localStorage.getItem('unit_search.unit_name');
+
+		this.searchForm = new FormGroup({
+            'unit_code': new FormControl(this.unitCode),
+            'unit_name': new FormControl(this.unitName),
+        });
+	}
+
+	//----------------------------------------------------------------
+	// event on Init
+	//----------------------------------------------------------------
+	ngOnInit() {
+
+		this._set_init();
+		
+		// If the user changes the sort order, reset back to the first page.
+		this.sort.sortChange.subscribe(() => this.paginator.pageIndex = 0);
+
+		merge(this.sort.sortChange,this.paginator.page)
+			.pipe(
+				startWith({}),
+				switchMap(()=>{
+					this.isLoadingResults = true;
+					return this._set_search_data();
+				}),
+				map(data =>{
+					this.isLoadingResults	= false;
+					this.isRateLimitReached	= false;
+
+					if (data['status'] == 'error'){
+						this.snackBar.openFromComponent(MyMessageComponent,{
+							data: [data['message']],
+							duration:2000,
+							panelClass:['mat-snack-bar-container-message']
+						})
+						
+					}
+					
+					this.resultsLength		= data['data_count'];
+					return data['data'];
+				}),
+				catchError(error => {
+					this.isLoadingResults = false;
+					console.log(error);
+					// Catch if the GitHub API has reached its rate limit. Return empty data.
+					this.isRateLimitReached = true;
+					return observableOf([]);
+				})
+			)
+			.subscribe(data=>{
+				this.dataSource = new MatTableDataSource(data);
+			}
+		);
+
+	}
+
+	//----------------------------------------------------------------
+	// event on search click
+	//----------------------------------------------------------------
+	onSearchClick(){
+		localStorage.setItem('unit_search.unit_code',this.searchForm.controls['unit_code'].value);
+		localStorage.setItem('unit_search.unit_name',this.searchForm.controls['unit_name'].value);
+		this.ngOnInit();
+	}
+
+
+	//----------------------------------------------------------------
+	// event on clear click
+	//----------------------------------------------------------------
+	onClear(){
+		this._clear_data();
+		this.ngOnInit();
+	}
+
+	//----------------------------------------------------------------
+	// event on add click
+	//----------------------------------------------------------------
+	onAdd(){
+		localStorage.setItem('unit_input.id','-1');
+		this.router.navigateByUrl('unit_input');
+	}
+
+	//----------------------------------------------------------------
+	// event on edit
+	//----------------------------------------------------------------
+	onEdit(id:number){
+		localStorage.setItem('unit_input.id',String(id));
+		this.router.navigateByUrl('unit_input');
+	}
+
+	//----------------------------------------------------------------
+	// event on delete
+	//----------------------------------------------------------------
+	onDelete(id:number){
+		const dialogRef = this.dialog.open(ShowDialogComponent,{
+			width: '350px',
+			height: '200px',
+			data: {description:'คุณต้องการลบรายการนี้จริิงหรือเปล่า',id:id}
+		})
+		
+		dialogRef.afterClosed().subscribe(result=>{
+			if (!result){
+				//cancel delete data
+				alert('hiii');
+			} else {
+				//console.log(result);
+				//start delete data
+				localStorage.setItem('unit_input.delete_id',result);
+				this.unitS.deleteById().subscribe(data =>{
+					if (data['status'] == 'success'){
+						this.snackBar.openFromComponent(MyMessageComponent,{
+							data:['ทำการลบรายการแล้ว'],
+							duration:5000,
+							panelClass:['mat-snack-bar-container-message']
+						})
+						this.ngOnInit();
+					} else {
+						console.log(data['message']);
+					}
+				});
+			}
+		})
+	}
+
+}
+
+export interface ListElement {
+	id:number;
+	unit_code: string;
+	unit_name: string;
+	remark: string;
 }
--- WEB/src/app/master/item-type/item-type.service.ts
+++ WEB/src/app/master/item-type/item-type.service.ts
@@ -1,9 +1,70 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { SortDirection } from '@angular/material';
+
+const BASE_URL	= environment.api_url+'/UnitController';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ItemTypeService {
+	constructor(private http:HttpClient) { }
 
-  constructor() { }
+	public getListData(search_data:cSearch){
+		search_data.token	= localStorage.getItem('token');
+
+		let strJSON:string = JSON.stringify(search_data);
+		return this.http.post(BASE_URL+'/get_data_list',strJSON);
+	}
+
+	public getById(){
+		let search_data:cGetById	= new cGetById();
+		search_data.token			= localStorage.getItem('token');
+		search_data.id				= +localStorage.getItem('unit_input.id');
+
+		let strJSON:string = JSON.stringify(search_data);
+		return this.http.post(BASE_URL+'/get_data_by_id',strJSON);
+		
+	}
+
+	public updateById(inputData:cInput){
+		inputData.token	= localStorage.getItem('token');
+		let strJSON:string = JSON.stringify(inputData);
+		return this.http.post(BASE_URL+'/update_data',strJSON);
+	}
+
+	public deleteById(){
+		let search_data:cGetById	= new cGetById();
+		search_data.token			= localStorage.getItem('token');
+		search_data.id				= +localStorage.getItem('unit_input.delete_id');
+		console.log(search_data);
+		let strJSON:string = JSON.stringify(search_data);
+		return this.http.post(BASE_URL+'/delete_data_by_id',strJSON);
+	}
+}
+
+
+
+export class cGetById{
+	token:string;
+	id:number;
+}
+
+export class cSearch{
+	token:string;
+	sort_column:string;
+	sort_direction:SortDirection;
+	page_index:number;
+	page_size:number;
+	unit_code:string;
+	unit_name:string;
+}
+
+export class cInput{
+	token:string;
+	id:number;
+	unitCode:string;
+	unitName:string;
+	remark:string;
 }

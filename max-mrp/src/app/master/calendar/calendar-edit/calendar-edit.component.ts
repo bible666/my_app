15604c9f447bd8513181ea8e36316f49a41e84fc
egg-staff <rattapol@egg.co.th>
2020-04-06 10:17:25 +0700

    update code for add calendar in form only

--- max-mrp/src/app/master/calendar/calendar-edit/calendar-edit.component.ts
+++ max-mrp/src/app/master/calendar/calendar-edit/calendar-edit.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators, FormArray } from '@angular/forms';
+import { MessageService, MessageClass } from '../../../service/message.service';
+import { cInput, CarlendarService } from '../../../service/carlendar.service';
+import { Router, ActivatedRoute } from '@angular/router';
+
 
 @Component({
   selector: 'app-calendar-edit',
@@ -7,9 +12,102 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalendarEditComponent implements OnInit {
 
-  constructor() { }
+  public message: MessageClass[] = [];
+  submitted: boolean = false;
 
-  ngOnInit(): void {
+  //----------------------------------------------------------------
+  // set local Valiable
+  //----------------------------------------------------------------
+  id      : string;
+  
+  inputForm = new FormGroup({
+    'currency_code'       : new FormControl('', [ Validators.required, Validators.maxLength(10) ]),
+    'currency_name'       : new FormControl('', [ Validators.required, Validators.maxLength(50) ]),
+    'default_currency'    : new FormControl(false),
+    'remark'              : new FormControl('', [ Validators.maxLength(200) ]),
+    'holidays'             : new FormArray([])
+  });
+  
+  constructor(private param: ActivatedRoute,
+    private Service: CarlendarService,
+    private ServiceMessage: MessageService,
+    private router: Router) { }
+
+  ngOnInit() {
+    this.id	= this.param.snapshot.params.id;
+
+    if (this.id != '-1') {
+      //get data from database
+      this.Service.getDataById(this.id)
+      .subscribe(data=>{
+        
+        if (data['status']== 'success'){
+          console.log(data);
+          let isDefault = true;
+          if (data['data'].default_currency == 0){
+            isDefault = false;
+          }
+          this.inputForm.patchValue({
+            'currency_code'    : data['data'].currency_code,
+            'currency_name'    : data['data'].currency_name,
+            'default_currency' : isDefault,
+            'remark'           : data['data'].remark
+          });
+        } else {
+          this.ServiceMessage.setError(data['message']);
+          this.message = this.ServiceMessage.getMessage();
+        }
+        
+      },
+        error=>{
+          this.ServiceMessage.setError('เกิดข้อผิดพลาดไม่สามารถดึงข้อมูลได้');
+          this.message = this.ServiceMessage.getMessage();
+        });
+      }
+  }
+
+  get holidayForms(){
+    return this.inputForm.get('holidays') as FormArray
+  }
+
+  onAddHoliday(){
+    let holiday = new FormGroup({
+      'holiday_date'        : new FormControl('', [ Validators.required, Validators.maxLength(10) ]),
+      'holiday_name'        : new FormControl('', [ Validators.required, Validators.maxLength(50) ]),
+    });
+
+    this.holidayForms.push(holiday);
+  }
+
+  onDeleteHoliday(i){
+    this.holidayForms.removeAt(i);
+  }
+
+  onSubmit(){
+    this.submitted = true;
+    if (this.inputForm.invalid){
+      return;
+    }
+
+    let input_data	: cInput = new cInput(this.inputForm.value);
+    input_data.id	= this.id;
+    this.Service.updateById(input_data)
+    .subscribe(data=>{
+
+      if (data['status']== 'success'){
+        this.ServiceMessage.setSuccess('บันทึกสำเร็จ');
+        this.router.navigateByUrl('/currency/list');
+			} else {
+        this.ServiceMessage.setError(data['message']);
+        this.message = this.ServiceMessage.getMessage();
+      }
+			
+    },
+    error=>{
+      this.ServiceMessage.setError('บันทึกผิดพลาด');
+      this.message = this.ServiceMessage.getMessage();
+    });
   }
 
 }
+
--- max-mrp/src/app/service/carlendar.service.ts
+++ max-mrp/src/app/service/carlendar.service.ts
@@ -1,9 +1,80 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+
+const BASE_URL = environment.api_url+'/CurrencyController';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type'  : 'application/json',
+    'Authorization' : sessionStorage.getItem('token')
+  })
+};
 
 @Injectable({
   providedIn: 'root'
 })
 export class CarlendarService {
 
-  constructor() { }
+  constructor(
+    private http:HttpClient
+  ) { }
+
+  public getListData(search_data:cSearch){
+    let strJSON:string = JSON.stringify(search_data);
+    return this.http.post(BASE_URL+'/get_data_list',strJSON,httpOptions);
+  }
+
+  public deleteById(id:string){
+    let inputData = new cInput();
+    inputData.id = id;
+    let strJSON:string  = JSON.stringify(inputData);
+    return this.http.post(BASE_URL+'/delete_data_by_id',strJSON,httpOptions);
+  }
+
+  public getDataById(id:string){
+    let inputData = new cInput();
+    inputData.id = id;
+    let strJSON:string  = JSON.stringify(inputData);
+    return this.http.post(BASE_URL+'/get_data_by_id',strJSON,httpOptions);
+  }
+
+  public updateById(inputData:cInput){
+    let strJSON:string  = JSON.stringify(inputData);
+    return this.http.post(BASE_URL+'/update_data',strJSON,httpOptions);
+  }
+}
+
+
+export class cSearch{
+  page_index  :number;
+  rowsPerpage   :number;
+
+  //manual search condition
+  currency_code   :string;
+  description :string;
+
+  public constructor(init?: Partial<cSearch>){
+      Object.assign(this,init);
+   }
+}
+
+export class cData{
+  currency_code : string;
+  description : string;
+}
+
+export class cInput{
+  token         : string;
+  id            : string;
+
+  currency_id       : string;
+  currency_code     : string;
+  currency_name     : string;
+  default_currency  : boolean;
+  remark            : string;
+
+  public constructor(init?: Partial<cInput>){
+    Object.assign(this,init);
+  }
 }

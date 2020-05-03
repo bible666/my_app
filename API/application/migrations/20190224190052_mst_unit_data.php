468e8a7c5ae1777f145f9e3064216f973be994f5
egg-staff <rattapol@egg.co.th>
2020-05-03 15:23:33 +0700

    unit data update

---
+++ API/application/migrations/20190224190052_mst_unit_data.php
@@ -0,0 +1,33 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Migration_mst_unit_data extends CI_Migration {
+
+    public function up()
+    {
+        $this->db->query("
+            insert into mst_unit
+            (unit_code,unit_name,active_flag,create_date,create_user)
+            values
+            ('cm','เซนติเมตร',true,now(),1),
+            ('can','กระป๋อง',true,now(),1),
+            ('kg','กิโลกรัม',true,now(),1),
+            ('l','ลิตร',true,now(),1),
+            ('m','เมตร',true,now(),1),
+            ('mm','มิลลิเมตร',true,now(),1),
+            ('pcs','ชิ้น',true,now(),1),
+            ('m²','ตารางเมตร',true,now(),1);
+            
+        ");
+
+    }
+
+    public function down()
+    {
+        $this->db->query("
+            delete mst_unit;
+        ");
+    }
+}
+?>

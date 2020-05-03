61563a559585b94d0821c83d1395c9a5d9849f46
egg-staff <rattapol@egg.co.th>
2020-05-03 13:00:29 +0700

    update

--- max-mrp/src/app/system/system.module.ts
+++ max-mrp/src/app/system/system.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedCommonModule } from '../common/common.module'
 import { SharedTemplateModule } from '../template/template.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { SystemRoutingModule } from './system-routing.module';
 import { UnitListComponent } from './unit-list/unit-list.component';
@@ -9,12 +10,15 @@ import { UnitEditComponent } from './unit-edit/unit-edit.component';
 
 
 @NgModule({
-  declarations: [UnitListComponent, UnitEditComponent],
+  declarations: [
+    UnitListComponent, UnitEditComponent
+  ],
   imports: [
     CommonModule,
     SystemRoutingModule,
     SharedCommonModule,
-    SharedTemplateModule
+    SharedTemplateModule,
+    FormsModule, ReactiveFormsModule
   ]
 })
 export class SystemModule { }

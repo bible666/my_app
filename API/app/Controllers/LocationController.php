06e63be185eb61052cf2b92aa95551938d5e7431
egg-staff <rattapol@egg.co.th>
2020-05-06 23:06:55 +0700

    update location api

--- API/app/Controllers/LocationController.php
+++ API/app/Controllers/LocationController.php
@@ -1,30 +1,45 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-require('Origin001.php');
+namespace App\Controllers;
+
+use Config\App;
 
 class LocationController extends Origin001
 {
+	protected $format    = 'json';
+    
+	protected $mst_factory;
+	protected $mst_location;
+
 	/**
 	 * Constructure class
 	 */
-	public function __construct()
+    public function initController(\CodeIgniter\HTTP\RequestInterface $request, \CodeIgniter\HTTP\ResponseInterface $response, \Psr\Log\LoggerInterface $logger)
 	{
-		parent::__construct();
-		$this->load->helper('date');
-		$this->load->database();
+
+		// Do Not Edit This Line
+		parent::initController($request, $response, $logger);
+
+		//--------------------------------------------------------------------
+		// Preload any models, libraries, etc, here.
+		//--------------------------------------------------------------------
+		// E.g.:
+        // $this->session = \Config\Services::session();
+
+		$this->mst_location        = $this->db->table('mst_location');
+		$this->mst_factory        = $this->db->table('mst_factory');
 		
 	}
 
 	/**
      * delete data by id
      */
-    public function delete_data_by_id_post(){
-		$data       = $this->post();
+    public function delete_data_by_id(){
+		$data       = $this->request->getJSON();
  
         //init data
 		$token				= $this->getAuthHeader();
-		$location_cd         = isset($data['location_code']) ? $data['location_code'] : -1;
-        $factory_cd         = isset($data['factory_code']) ? $data['factory_code'] : -1;
+		$location_cd        = isset($data->location_code) ? $data->location_code : -1;
+        $factory_cd         = isset($data->factory_code) ? $data->factory_code : -1;
 
 		$result     = $this->_checkToken($token);
 		//print_r($result);
@@ -33,11 +48,11 @@ class LocationController extends Origin001
 			$insert_data['update_date']     = date("Y-m-d H:i:s");
             $insert_data['update_user']     = $result->user_id;
 
-            $this->db->where([
+            $where = [
 				'factory_code'			=> $factory_cd,
 				'location_code'			=> $location_cd
-			]);
-            $this->db->update('mst_location',$insert_data);
+			];
+            $this->mst_location->update($insert_data,$where);
             
             $dataDB['status']   = "success";
             $dataDB['message']  = "";
@@ -48,31 +63,31 @@ class LocationController extends Origin001
             $dataDB['message']  = "token not found";
             $dataDB['data']     = "";
         }
-        $this->response($dataDB,200);
+        return $this->respond($dataDB,200);
     }
 
 	/**
 	 * get data by id
 	 */
-	public function get_data_by_id_post(){
+	public function get_data_by_id(){
 		$token		= $this->getAuthHeader();
-		$data       = $this->post();
+		$data       = $this->request->getJSON();
 
 		//init data
-		$location_cd         = isset($data['location_code']) ? $data['location_code'] : -1;
-        $factory_cd         = isset($data['factory_code']) ? $data['factory_code'] : -1;
+		$location_cd        = isset($data->location_code) ? $data->location_code : -1;
+        $factory_cd         = isset($data->factory_code) ? $data->factory_code : -1;
 
 		$result     = $this->_checkToken($token);
 		if($result->user_id > 0){
 			$query_str = "
 			SELECT *
 			FROM mst_location
-			WHERE factory_code = '{$factory_cd}'
-				AND location_code = '{$location_cd}'
+			WHERE factory_code = :factory_cd:
+				AND location_code = :location_cd:
 				AND active_flag = true
 			";
 
-			$itemn_data = $this->db->query($query_str)->row();
+			$itemn_data = $this->db->query($query_str,['factory_cd'=>$factory_cd,'location_cd'=>$location_cd])->getRow();
 			$dataDB['status']   = "success";
 			$dataDB['message']  = $query_str;
 			$dataDB['data']     = $itemn_data;
@@ -82,7 +97,7 @@ class LocationController extends Origin001
 			$dataDB['message']  = "token not found";
 			$dataDB['data']     = "";
 		}
-		$this->response($dataDB,200);
+		return $this->respond($dataDB,200);
 	}
 
 
@@ -131,32 +146,32 @@ class LocationController extends Origin001
         return [$strCond,$params];
     }
 
-	public function get_factory_post(){
+	public function get_factory(){
 		$token		= $this->getAuthHeader();
 
 		$query_str = " SELECT * FROM mst_factory where active_flag = true";
 
-		$itemn_data = $this->db->query($query_str)->result();
+		$itemn_data = $this->db->query($query_str)->getResult();
 
 		$dataDB['status']   = "success";
 		$dataDB['message']  = "";
 		$dataDB['data']     = $itemn_data;
 
-		$this->response($dataDB,200);
+		return $this->respond($dataDB,200);
 	}
 
 	/**
 	 * get list data
 	 */
-	public function get_data_list_post(){
-		$data       = $this->post();
+	public function get_data_list(){
+		$data       = $this->request->getJSON();
 		$token		= $this->getAuthHeader();
 
 		//Validate Data
 
 
-		$limit		= intval($data['rowsPerpage']);
-		$offset		= ($data['page_index']-1) * $limit;
+		$limit		= intval($data->rowsPerpage);
+		$offset		= ($data->page_index-1) * $limit;
 
 		$result     = $this->_checkToken($token);
 		if($result->user_id >= 0){
@@ -174,15 +189,15 @@ class LocationController extends Origin001
 			";
 			//print_r($query_str);exit;
 			$query_count = "
-			SELECT count(factory_code) as my_count
+			SELECT count(location_code) as my_count
 			FROM mst_location
 			WHERE ". $strCond." active_flag = true
 			ORDER BY factory_code,location_code
 			";
 			
-			$itemn_data = $this->db->query($query_str,[$result->company_id])->result();
+			$itemn_data = $this->db->query($query_str,[$result->company_id])->getResult();
 
-			$itemn_count = $this->db->query($query_count, [$result->company_id])->result();
+			$itemn_count = $this->db->query($query_count, [$result->company_id])->getResult();
 
 			$dataDB['status']   = "success";
 			$dataDB['message']  = "";
@@ -194,64 +209,65 @@ class LocationController extends Origin001
 			$dataDB['message']  = "token not found";
 			$dataDB['data']     = "";
 		}
-		$this->response($dataDB,200);
+		return $this->respond($dataDB,200);
 	}
 	
 
 	/**
 	 * à¸µupdate / insert data to database
 	 */
-	public function update_data_post(){
+	public function update_data(){
+		
 		$token			= $this->getAuthHeader();
-		$data           = $this->post();
-
+		$data           = $this->request->getJSON();
+		
 		//init data
-		$old_location  		= isset($data['old_location'])	? $data['old_location']			: -1;
-		$old_factory  		= isset($data['old_factory'])	? $data['old_factory']			: -1;
+		$old_location  		= isset($data->old_location)	? $data->old_location			: -1;
+		$old_factory  		= isset($data->old_factory)	? $data->old_factory			: -1;
 
 		
-		$factory_code		= isset($data['factory_code'])	? $data['factory_code'] : '';
-		$location_code		= isset($data['location_code'])	? $data['location_code'] : '';
-		$location_name		= isset($data['location_name'])	? $data['location_name'] : '';
+		$factory_code		= isset($data->factory_code)	? $data->factory_code : '';
+		$location_code		= isset($data->location_code)	? $data->location_code : '';
+		$location_name		= isset($data->location_name)	? $data->location_name : '';
 		
-        $mrp_flag             = isset($data['mrp_flag'])		? $data['mrp_flag']       : '';
-        $expire_flag             = isset($data['expire_flag'])		? $data['expire_flag']       : '';
+        $mrp_flag             = isset($data->mrp_flag)		? $data->mrp_flag       : '';
+        $expire_flag             = isset($data->expire_flag)		? $data->expire_flag       : '';
 	
-		$remark         	= isset($data['remark'])		? $data['remark']       : '';
+		$remark         	= isset($data->remark)		? $data->remark       : '';
 
 		//Validation Data
 		if ( $token == '') {
 			$dataDB['status']   = "error";
 			$dataDB['message']  = "token is empty";
 			$dataDB['data']     = "";
-			$this->response($dataDB,200);
+			return $this->respond($dataDB,200);
 		}
 
 		if ( $location_name == '') {
 			$dataDB['status']   = "error";
 			$dataDB['message']  = "กรุณาระบุชื่อพื้นที่เก็บของ";
 			$dataDB['data']     = "";
-			$this->response($dataDB,200);
+			return $this->respond($dataDB,200);
 		}
 
 		if ( $location_code == '') {
 			$dataDB['status']   = "error";
 			$dataDB['message']  = "กรุณาระบุรหัสพื้นที่เก็บของ";
 			$dataDB['data']     = "";
-			$this->response($dataDB,200);
+			return $this->respond($dataDB,200);
 		}
 
 
 		//get data from token
 		$result     = $this->_checkToken($token);
-
+		
 		if($result->user_id > 0){
 
 			if ($this->is_dupplicate_data($old_factory,$old_location, $factory_code,$location_code)){
 				$dataDB['status']   = "error";
 				$dataDB['message']  = "รหัสนี้ใช้งานแล้ว";
 				$dataDB['data']     = "";
-				$this->response($dataDB,200);
+				return $this->respond($dataDB,200);
 
 			}
 
@@ -269,19 +285,19 @@ class LocationController extends Origin001
 			$insert_data['active_flag']		= true;
 			
 
-			$this->db->trans_start();
+			$this->db->transStart();
 
 			if ($old_location == '-1' ){
 				$insert_data['create_date']        = date("Y-m-d H:i:s");
 				$insert_data['create_user']        = $result->user_id;
-				$this->db->insert('mst_location', $insert_data);
+				$this->mst_location->insert( $insert_data);
 			}else{
 				$insert_data['update_date']    = date("Y-m-d H:i:s");
 				$insert_data['update_user']    = $result->user_id;
 
-				$this->db->update('mst_location',$insert_data,['factory_code' => $old_location, 'location_code' => $old_factory]);
+				$this->mst_location->update($insert_data,['factory_code' => $old_location, 'location_code' => $old_factory]);
 			}
-			$this->db->trans_complete();
+			$this->db->transComplete();
 
 			$dataDB['status']   = "success";
 			$dataDB['message']  = "";
@@ -291,7 +307,7 @@ class LocationController extends Origin001
 			$dataDB['message']  = "token not found";
 			$dataDB['data']     = "";
 		}
-		$this->response($dataDB,200);
+		return $this->respond($dataDB,200);
 	}
 
 	/**
@@ -309,12 +325,13 @@ class LocationController extends Origin001
 			return false; // OK data
 		}
 
-		$data	= $this->db->get_where('mst_location',[
-			'factory_code'	=> $factory,
-			'factory_code !=' => $old_factory,
-			'location_code' => $location,
-			'location_code != ' => $old_location
-		])->row();
+		$data	= $this->mst_location->getWhere([
+			'factory_code'	    => $factory,
+			'factory_code !='   => $old_factory,
+			'location_code'     => $location,
+			'location_code != ' => $old_location,
+			'active_flag'       => true
+		])->getRow();
 
 		if (isset($data)){
 

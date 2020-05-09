b7eea1600336c03fe5fb3e51d1965e1b10e08c61
egg-staff <rattapol@egg.co.th>
2020-05-09 21:32:23 +0700

    update

--- max-mrp/src/app/purchase/purchase-routing.module.ts
+++ max-mrp/src/app/purchase/purchase-routing.module.ts
@@ -1,15 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { SupplierListComponent } from './supplier/supplier-list/supplier-list.component';
 import { AuthGuard } from '../service/auth.guard';
-import { ItemListComponent } from './item-list/item-list.component';
-import { ItemEditComponent } from './item-edit/item-edit.component';
+
 
 
 const routes: Routes = [
-  { path: 'supplier/list',               component:SupplierListComponent,canActivate:[AuthGuard]},
-  { path: 'item/list',               component:SupplierListComponent,canActivate:[AuthGuard]},
-  { path: 'item/edit/:item_code',               component:SupplierListComponent,canActivate:[AuthGuard]},
+
 ];
 
 @NgModule({

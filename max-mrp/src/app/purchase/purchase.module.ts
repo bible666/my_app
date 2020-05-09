e03e20d7aad22ff468a73adb276b1c683d1bf52f
egg-staff <rattapol@egg.co.th>
2020-05-09 21:07:36 +0700

    remove unuse file

--- max-mrp/src/app/purchase/purchase.module.ts
+++ max-mrp/src/app/purchase/purchase.module.ts
@@ -2,14 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PurchaseRoutingModule } from './purchase-routing.module';
-import { SupplierListComponent } from './supplier/supplier-list/supplier-list.component';
-import { ItemListComponent } from './item-list/item-list.component';
-import { ItemEditComponent } from './item-edit/item-edit.component';
 import { SharedCommonModule } from '../common/common.module'
 
 
 @NgModule({
-  declarations: [SupplierListComponent, ItemListComponent, ItemEditComponent],
+  declarations: [],
   imports: [
     CommonModule,
     PurchaseRoutingModule,

111f4df5bc6e86f362561c67d7a43311143624b4
egg-staff <rattapol@egg.co.th>
2020-05-11 16:34:53 +0700

    update api for support excel

--- API/app/Controllers/Home.php
+++ API/app/Controllers/Home.php
@@ -1,9 +1,21 @@
 <?php namespace App\Controllers;
 
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+
 class Home extends BaseController
 {
 	public function index()
 	{
+		$spreadsheet = new Spreadsheet();
+		$sheet = $spreadsheet->getActiveSheet();
+		$sheet->setCellValue('A1', 'Hello World !');
+        
+        $writer = new Xlsx($spreadsheet);
+ 
+        $filename = 'name-of-the-generated-file.xlsx';
+ 
+        $writer->save($filename); // will c
 		return view('welcome_message');
 	}
 

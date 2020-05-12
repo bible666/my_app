839b00ddbc78de80bfa3315517845a920d42f8c7
egg-staff <rattapol@egg.co.th>
2020-05-12 09:26:52 +0700

    update sample code download excel

--- API/app/Controllers/Home.php
+++ API/app/Controllers/Home.php
@@ -19,6 +19,24 @@ class Home extends BaseController
 		return view('welcome_message');
 	}
 
+	public function download()
+    {
+        $spreadsheet = new Spreadsheet();
+        $sheet = $spreadsheet->getActiveSheet();
+        $sheet->setCellValue('A1', 'Hello World !');
+        
+        $writer = new Xlsx($spreadsheet);
+ 
+        $filename = 'name-of-the-generated-file';
+ 
+        header('Content-Type: application/vnd.ms-excel');
+        header('Content-Disposition: attachment;filename="'. $filename .'.xlsx"'); 
+        header('Cache-Control: max-age=0');
+        
+        $writer->save('php://output'); // download file 
+ 
+    }
+
 	//--------------------------------------------------------------------
 
 }

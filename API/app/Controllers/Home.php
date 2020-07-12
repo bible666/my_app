fe1c8c9680a2ddf875c0277304ac92e842c5f277
egg-staff <rattapol@egg.co.th>
2020-07-12 18:26:07 +0700

    update

--- API/app/Controllers/Home.php
+++ API/app/Controllers/Home.php
@@ -1,21 +1,76 @@
 <?php namespace App\Controllers;
 
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Spreadsheet\IOFactory;
 use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+use PhpOffice\PhpSpreadsheet\Chart\Chart;
+use PhpOffice\PhpSpreadsheet\Chart\DataSeries;
+use PhpOffice\PhpSpreadsheet\Chart\DataSeriesValues;
+use PhpOffice\PhpSpreadsheet\Chart\Layout;
+use PhpOffice\PhpSpreadsheet\Chart\Legend;
+use PhpOffice\PhpSpreadsheet\Chart\PlotArea;
+use PhpOffice\PhpSpreadsheet\Chart\Title;
+
+use PhpOffice\PhpSpreadsheet\Writer\WriterPart;
 
 class Home extends BaseController
 {
 	public function index()
 	{
-		$spreadsheet = new Spreadsheet();
-		$sheet = $spreadsheet->getActiveSheet();
-		$sheet->setCellValue('A1', 'Hello World !');
+    //     $inputFileName = './AAA.xlsx';
+    //     $inputFileType = 'Xlsx';
+    //     /**  Create a new Reader of the type defined in $inputFileType  **/
+    //     //$spreadsheet = \PhpOffice\PhpSpreadsheet\IOFactory::load("AAA.xlsx");
+    //     $reader = \PhpOffice\PhpSpreadsheet\IOFactory::createReader($inputFileType);
+    //     $reader->setIncludeCharts(true);
+    //     $spreadsheet = $reader->load($inputFileName);
+
+    //     $sheet = $spreadsheet->getActiveSheet();
+    //     //print_r($sheet->getChartNames());
+    //     //print_r($sheet->getChartByIndex(0)->getPlotArea()->getPlotSeriesCount());
+    //     //echo "//";
+    //    // print_r($sheet->getChartByIndex(0)->getPlotArea()->plotSeries);
+    //     /**  Load $inputFileName to a Spreadsheet Object  **/
+    //     //$spreadsheet = $reader->load($inputFileName);
+	// 	// $spreadsheet = new Spreadsheet();
+	// 	// $sheet = $spreadsheet->getActiveSheet();
+    //     // $sheet->setCellValue('A1', 'Hello World !');
+
         
-        $writer = new Xlsx($spreadsheet);
- 
-        $filename = 'name-of-the-generated-file.xlsx';
- 
-        $writer->save($filename); // will c
+    //     $writer = \PhpOffice\PhpSpreadsheet\IOFactory::createWriter($spreadsheet, 'Xlsx');
+    //     $writer->setIncludeCharts(true); // useless since no charts were loaded
+    //     $writer->save('template2.xlsx'); // cell A1 has value 123, but the chart is gone
+
+        //$filename = 'name-of-the-generated-file.xlsx';
+        if (!copy('./AAA.xlsx','bbb.xlsx')){
+            echo 'error';
+            exit;
+        }
+
+        $inputFileName = './bbb.xlsx';
+        $inputFileType = 'Xlsx';
+        /**  Create a new Reader of the type defined in $inputFileType  **/
+        //$spreadsheet = \PhpOffice\PhpSpreadsheet\IOFactory::load("AAA.xlsx");
+        $reader = \PhpOffice\PhpSpreadsheet\IOFactory::createReader($inputFileType);
+        $reader->setIncludeCharts(true);
+        $spreadsheet = $reader->load('bbb.xlsx');
+
+        $sheet = $spreadsheet->getActiveSheet();
+        //print_r($sheet->getChartNames());
+        //print_r($sheet->getChartByIndex(0)->getPlotArea()->getPlotSeriesCount());
+        //echo "//";
+       // print_r($sheet->getChartByIndex(0)->getPlotArea()->plotSeries);
+        /**  Load $inputFileName to a Spreadsheet Object  **/
+        //$spreadsheet = $reader->load($inputFileName);
+		//$spreadsheet = new Spreadsheet();
+		//$sheet = $spreadsheet->getActiveSheet();
+        $sheet->setCellValue('A1', 'Hello World5555 !');
+      
+        $writer = \PhpOffice\PhpSpreadsheet\IOFactory::createWriter($spreadsheet, 'Xlsx');
+        $writer->setIncludeCharts(true); // useless since no charts were loaded
+        $writer->save('ccc.xlsx'); // cell A1 has value 123, but the chart is gone
+
+        //$writer->save($filename); // will c
 		return view('welcome_message');
 	}
 
@@ -40,3 +95,4 @@ class Home extends BaseController
 	//--------------------------------------------------------------------
 
 }
+
